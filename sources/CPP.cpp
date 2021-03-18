5172576758340ebdaca6fb16e0c8ce637d55ba41
legik777 <75944488+legik777@users.noreply.github.com>
2021-03-18 19:18:38 +0300

    Update CPP.cpp

--- sources/CPP.cpp
+++ sources/CPP.cpp
@@ -56,13 +56,15 @@ void TF(std::shared_ptr<std::mutex> mutex, int i, unsigned int64 startingPoint){
       mutex->lock();
       std::chrono::time_point now = std::chrono::high_resolution_clock::now();
       unsigned int64 timespan = now.time_since_epoch().count() - startingPoint;
-      BOOST_LOG_TRIVIAL(info) << i << " " << timespan << " " << randomstr << ": " << hexString << std::endl;
+      BOOST_LOG_TRIVIAL(info) << i << " " << timespan << " " << randomstr
+          << ": " << hexString << std::endl;
       OutputJSON(timespan, hexString, randomstr);
       mutex->unlock();
         }else{
 mutex->lock();
 std::chrono::time_point now = std::chrono::high_resolution_clock::now();
-unsigned int timespan = now.time_since_epoch().count() - startingPoint;
+unsigned int timespan =
+    now.time_since_epoch().count() - startingPoint;
 BOOST_LOG_TRIVIAL(trace) << i << " " << randomstr << ": " << hexString << std::endl;
 OutputJSON(timespan, hexString, randomstr);
 mutex->unlock();
